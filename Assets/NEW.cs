9a7cdec8e8e512602882f2837b603414f062a00b
pepegon08 <166547058+pepegon08@users.noreply.github.com>
2025-04-23 15:54:54 -0300

    todo

---
+++ Assets/NEW.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class NEW : MonoBehaviour
+{
+
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+    public TMP_InputField inputUsuario;
+
+    string clave = "tic2025";
+    string usuario = "pepegon08";
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMensaje.text = string.Empty;
+    }
+
+
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inputClave.text;
+        if (claveIngresada != clave)
+        {
+            txtMensaje.text = "accesp denegado";
+            inputClave.text = string.Empty;
+            inputUsuario.text = string.Empty;
+        }
+        else
+        {
+            txtMensaje.text = "bienvenido";
+        }
+    }
+}
